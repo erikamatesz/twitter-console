3a1a3e232c5188a06efba1c618e72b42bd29fa17
Erika Bueno <erika.bueno@radixeng.com.br>
2019-01-30 15:27:50 -0200

    Add help and keys

--- commands/help.rb
+++ commands/help.rb
@@ -1,4 +1,9 @@
 module TweetConsole
     def self.help
+        Printc.title "Command list", :green, 1, true
+        Printc.pair('Tweet something', 'Create a new tweet', :yellow, :white, 0.1)
+        Printc.pair('My profile', 'Your own profile and tweets', :yellow, :white, 0.1)
+        Printc.pair('Somebody else\'s profile', 'Another user\'s profile', :yellow, :white, 0.1)
+        Printc.pair('Search', 'Search texts and hashtags', :yellow, :white, 0.1)
     end
 end
